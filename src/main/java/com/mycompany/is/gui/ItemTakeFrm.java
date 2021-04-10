f8030355f6a10ab058f29123d43edb1781832e2a
Pablo <pablo.marquez88@gmail.com>
2021-04-10 11:19:29 -0300

    Fix names

--- src/main/java/com/mycompany/is/gui/ItemTakeFrm.java
+++ src/main/java/com/mycompany/is/gui/ItemTakeFrm.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ale.gui;
+package com.mycompany.is.gui;
 
 import javax.swing.JInternalFrame;
 
